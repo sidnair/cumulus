1896871371fa0c4b6a76355e82c6a26e5e0a253b
Sid Nair <sidnair09@gmail.com>
2015-06-23 16:05:00 -0700

    handle both OS X and Linux

--- cumulus.sh
+++ cumulus.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
 
-function sendMessage {
-  terminal-notifier -message $2 -title $1 -open $3 -contentImage $4 -sound Glass
-  # notify-send "Cumulus Error" "$1" -t 5000 --icon=dialog-error
-  # notify-send "$IMG" "$URL copied to clipboard" -t 3000 --icon=$IMG
+####################
+# Primitives
+####################
+
+function isMac {
+  uname | grep -q "Darwin"
 }
 
-function takeScreen {
-  screencapture -i ~/.cumulus/screen-$(date +"%m-%d-%Y-%H:%M:%S").png
-  # (cd ~/.cumulus && scrot -s)
+# Display notification. Arguments:
+# 1) title
+# 2) message
+# 3) url to open on click (optional, ignored on Linux)
+# 4) icon url (optional)
+function displayNotification {
+  if isMac; then
+    terminal-notifier -message $2 -title $1 -open $3 -contentImage $4 -sound Glass
+  else
+    notify-send $1 $2 -t 3000 --icon=$4
+    # notify-send "Cumulus Error" "$1" -t 5000 --icon=dialog-error
+    # notify-send "$IMG" "$URL copied to clipboard" -t 3000 --icon=$IMG
+  fi
+}
+
+function takeScreenshot {
+  if isMac; then
+    screencapture -i ~/.cumulus/screen-$(date +"%m-%d-%Y-%H:%M:%S").png
+  else
+    (cd ~/.cumulus && scrot -s)
+  fi
+}
+
+function clipboard {
+  if isMac; then
+    pbcopy
+  else
+    xsel -i -b
+  fi
 }
 
 function uploadImage {
@@ -18,24 +46,23 @@ function uploadImage {
   cat ~/.cumulus/.imgur-response | grep -o -P '"http.*?\"' | tr -d '\\"'
 }
 
-function clipboard {
-  pbcopy
-  # xsel -i -b
-}
-
 function error {
-  sendMessage "Cumulus Error" "$1" $URL
+  displayNotification "Cumulus Error" "$1"
   exit 1
 }
+
+####################
+# Main execution
+####################
 
 [ -e "$HOME/.cumulusrc" ] || error "Missing ~/.cumulusrc. Please create one with your imgur client id."
 
 mkdir -p ~/.cumulus || error "Can't initialize '~/.cumulus directory'"
-takeScreen || error 'Failed to take screenshot'
+takeScreenshot || error 'Failed to take screenshot'
 
 IMG=$(ls -t ~/.cumulus/*.png | head -n 1)
 URL=`uploadImage $IMG`
 echo $URL | clipboard
-sendMessage "$IMG" "$URL copied to clipboard" $URL $IMG
+displayNotification "$IMG" "$URL copied to clipboard" $URL $IMG
 
 exit 0
